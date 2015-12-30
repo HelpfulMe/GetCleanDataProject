6db7c4c1067330d402bf2fb618bd2e57e79e8fd6
Becca <beccasmithusa@gmail.com>
2015-12-29 19:19:42 -0500

    Progress
    making progress on run_analysis code.. up to cleaning up variable names

--- run_analysis.R
+++ run_analysis.R
@@ -29,7 +29,7 @@ options(stringsAsFactors = FALSE)
 # get user's working directory: 
 wd <- getwd()
 
-## test data
+## read test data
 # X_test.txt
 X_test <- read.table(file.path(wd, "test", "X_test.txt"))
 # y_test.txt
@@ -49,19 +49,21 @@ subj_train <- read.table(file.path(wd, "train", "subject_train.txt"))
 ## Assignment Part 1: 
 ## # 1.	Merges the training and the test sets to create one data set.
 
-# Use rbind to make one column for subjects
-subj <- rbind(subj_test, subj_train)
 
-# repeat for X (test variables)
+
+# use rbind to make one column for X (test variables)
 X <- rbind(X_test, X_train)
 
-# and repeat again for Y (activity labels)
+# repeat for Y (activity labels)
 Y <- rbind(Y_test, Y_train)
 
-# we have three groups, subj, X, and Y, all with 10299 observations
+# and repeat again for subjects
+subj <- rbind(subj_test, subj_train)
+
+# we have three groups, X, Y, and subj, all with 10299 observations
 # I will use cbind to combine them into one data set called 'data' to 
 # complete step 1.
-data <- cbind(subj, X, Y)
+data <- cbind(X, Y, subj)
 
 
 ## ============================================================================
@@ -70,14 +72,14 @@ data <- cbind(subj, X, Y)
 #           each measurement. 
 
 ## currently my "data" is in the dangerous state of having three columns with
-# identical names: V1.  To avoid problems in the next step, I'll rename
+# identical names: V1.  To clarify things, I'll rename
 # two of the columns so each has a unique name
 
-# rename subject column, the first column in the data: 
-names(data)[1] <- "subj"
+# rename activity column, the 'y' column, the second-to-last column in the data:
+names(data)[562] <- "activity"
 
-# and rename activity column, the last column in the data:
-names(data)[563] <- "activity"
+# and rename subject column, the last column in the data: 
+names(data)[563] <- "subj"
 
 ## I will use the dplyr library to manipulate the data:
 # load dplyr library
@@ -85,6 +87,67 @@ library("dplyr")
 
 # set up data as a data frame table so can manipulate with dplyr
 data_df <- tbl_df(data)
+
+# now use dplyr's 'select' option to select only the columns containing mean
+# or standard deviation, as well as last two columns containing activity
+# and subject information
+## note: as directions do not specify whether to include only features ending
+## in mean or std, or ones where the terms appear anywhere in the feature
+## description, this code pulls all the columns where "mean" or "std" appear
+## anywhere in the description
+
+data2 <- select(data_df, 1:6, 41:46, 81:86, 121:126, 161:166, 201:202, 214:215, 
+                227:228, 240:241, 253:254, 266:271, 294:296, 345:350, 373:375, 
+                424:429, 452:454, 503:504, 513, 516:517, 526, 529:530, 539, 
+                542:543, 552, 555:563)
+
+## ============================================================================
+## Assignment Part 3: 
+## # 3.	Uses descriptive activity names to name the activities in the data set
+
+# first I will make a data frame with activity labels
+# note: to make the data cleaner, capital letters have been changed to lowercase, 
+# and underscores removed
+
+activitylabels <- data.frame(id=1:6, activitydesc=c("walking", "walkingupstairs", 
+                             "walkingdownstairs", "sitting", "standing", 
+                             "laying"))
+
+# merge the activity labels with data2
+data2 <- merge(data2, activitylabels, by.x = "activity", by.y = "id")
+
+# i will now have two columns for activity: "activity" (id numbers) and 
+# "activitydesc" which I will clean up by deleting the "activity"(id number) column
+data2 <- select(data2, -(activity))
+
+## ============================================================================
+## Assignment Part 4:
+## # 4.	Appropriately labels the data set with descriptive variable names. 
+
+# First, I load all the variable names from features.txt using read.table
+features <- read.table(file.path(wd, "features.txt"))
+
+# set features up as a table dataframe so that can manipulate with dplyr
+features <- tbl_df(features)
+
+# then i select only the variables we are interested in 
+features <- features[features$V1 %in% c(1:6, 41:46, 81:86, 121:126, 
+                                              161:166, 201:202, 214:215, 
+                                              227:228, 240:241, 253:254, 
+                                              266:271, 294:296, 345:350, 373:375, 
+                424:429, 452:454, 503:504, 513, 516:517, 526, 529:530, 539, 
+                542:543, 552, 555:563), ]
+
+# for a tidy dataset I do not want any capital letters
+# I convert the features to all lowercase
+features <- tolower(features$V2)
+
+# I also don't wany any odd characters such as "(", ")", ", " or "-"
+features <- gsub("\\(", "", features)
+features <- gsub("\\)", "", features)
+features <- gsub("\\,", "", features)
+features <- gsub("\\-", "", features)
+
 
 ## In case I want to use tidyr:
 # load tidyr library
